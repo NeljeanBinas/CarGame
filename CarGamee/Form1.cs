50924bdc2eaa69dec87d32a178fdb94d4c5f2a28
ActuallyMe-trust <actuallyme136@gmail.com>
2026-09-13 23:21:25 +0800

    Add scrolling road background to CarGame Form

--- CarGamee/Form1.cs
+++ CarGamee/Form1.cs
@@ -1,11 +1,19 @@
 using System;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 
 namespace CarGamee
 {
     public partial class Form1 : Form
     {
+        // ROAD
+        private System.Windows.Forms.Timer timerRoad = null!;
+        private Image roadImage = null!;
+        private int roadWidth;
+        private int roadHeight;
+        private float roadY;
+
         public Form1()
         {
             InitializeComponent();
@@ -16,6 +24,7 @@ namespace CarGamee
         {
             InitilizeWindow();
             RegisterEvets();
+            InitializeRoad();
         }
 
         private void InitilizeWindow()
@@ -28,6 +37,29 @@ namespace CarGamee
             KeyPreview = true;
         }
 
+        private void InitializeRoad()
+        {
+            string projectPath = Directory.GetParent(
+                Application.StartupPath
+            )!.Parent!.Parent!.Parent!.FullName;
+
+            string roadPath = Path.Combine(
+                projectPath,
+                "Resources",
+                "road.png"
+            );
+
+            roadImage = Image.FromFile(roadPath);
+
+            roadWidth = ClientSize.Width;
+            roadHeight = ClientSize.Height;
+
+            timerRoad = new System.Windows.Forms.Timer();
+            timerRoad.Interval = 30;
+            timerRoad.Tick += TimerRoad_Tick;
+            timerRoad.Start();
+        }
+
         private void RegisterEvets()
         {
             Paint += Form1_Paint;
@@ -35,6 +67,19 @@ namespace CarGamee
             MouseMove += Form1_MouseMove;
             KeyUp += Form1_KeyUp;
             KeyDown += Form1_KeyDown;
+        }
+
+        private void TimerRoad_Tick(object? sender, EventArgs e)
+        {
+            roadY += 5;
+
+            if (roadY >= roadHeight)
+                roadY -= roadHeight;
+
+            if (roadY < 0)
+                roadY += roadHeight;
+
+            Invalidate();
         }
 
         private void Form1_KeyDown(object? sender, KeyEventArgs e)
@@ -55,6 +100,26 @@ namespace CarGamee
 
         private void Form1_Paint(object? sender, PaintEventArgs e)
         {
+            DrawRoad(e.Graphics);
+        }
+
+        private void DrawRoad(Graphics g)
+        {
+            g.DrawImage(
+                roadImage,
+                0,
+                roadY,
+                roadWidth,
+                roadHeight
+            );
+
+            g.DrawImage(
+                roadImage,
+                0,
+                roadY - roadHeight,
+                roadWidth,
+                roadHeight
+            );
         }
     }
 }
