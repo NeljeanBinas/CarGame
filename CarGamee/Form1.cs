f7d8234933378cf9b6e64b480cba2c3c96c8e678
ActuallyMe-trust <actuallyme136@gmail.com>
2026-09-15 13:40:00 +0800

    Add gameOver Logic

--- CarGamee/Form1.cs
+++ CarGamee/Form1.cs
@@ -9,6 +9,7 @@ namespace CarGamee
     {
         private Random rnd = new Random();
         private bool Collide = false;
+        private bool gameOver = false;
 
         //ROAD
         private System.Windows.Forms.Timer timerRoad = null!;
@@ -184,12 +185,12 @@ namespace CarGamee
 
             carFrames = new Rectangle[]
             {
-                new Rectangle(40,120,80,120),
-                new Rectangle(170,120,80,120),
-                new Rectangle(300,120,80,120),
+               new Rectangle(40,120,80,120),
+               new Rectangle(170,120,80,120),
+               new Rectangle(300,120,80,120),
 
-                new Rectangle(100,280,80,120),
-                new Rectangle(240,280,80,120)
+               new Rectangle(100,280,80,120),
+               new Rectangle(240,280,80,120)
             };
 
         }
@@ -330,6 +331,7 @@ namespace CarGamee
 
                 if (playerRect.IntersectsWith(enemyRect))
                 {
+                    gameOver = true;
                     Collide = true;
                     Invalidate();
                     return;
@@ -342,6 +344,23 @@ namespace CarGamee
                 }
 
             }
+        }
+
+        private void RestartGame()
+        {
+            gameOver = false;
+            roadY = 0;
+            speed = normalSpeed;
+            totalDistanceMeters = 0f;
+
+            playerX = lanes[1];
+            targetLane = 1;
+            currentLane = 1;
+
+            for (int i = 0; i < MAX_ENEMIES; i++)
+                enemyActive[i] = false;
+
+            timerRoad.Start();
         }
 
         //----------------------------- UPDATE METHODS -------------------------
@@ -470,6 +489,9 @@ namespace CarGamee
         //--------------------------- EVENT HANDLERS ---------------------------
         private void TimerRoad_Tick(object? sender, EventArgs e)
         {
+            if (gameOver)
+                return;
+
             UpdateSpeed();
             UpdateRoad();
             UpdatePlayerPosition();
@@ -480,6 +502,14 @@ namespace CarGamee
 
         private void Form1_KeyDown(object? sender, KeyEventArgs e)
         {
+            if (gameOver)
+            {
+                if ((e.KeyCode == Keys.Escape) ||
+                    (e.KeyCode == Keys.R) || (e.KeyCode == Keys.Enter))
+                    RestartGame();
+                return;
+            }
+
             if (choosingCar)
                 return;
 
@@ -526,6 +556,9 @@ namespace CarGamee
                 DrawCarSelection(e.Graphics);
             else
                 DrawPlayer(e.Graphics);
+
+            if (gameOver)
+                DrawGameOver(e.Graphics);
 
             DrawDebugInfo(e.Graphics);
         }
@@ -641,5 +674,36 @@ namespace CarGamee
                 g.DrawString(debugtext, font, Brushes.Yellow, 0, ClientSize.Height - 100);
             }
         }
+
+        private void DrawGameOver(Graphics g)
+        {
+            using (Brush overlay = new SolidBrush(Color.FromArgb(180, 0, 0, 0)))
+                g.FillRectangle(overlay, ClientRectangle);
+
+            using (Font title = new Font("Arial", 28, FontStyle.Bold))
+            using (Font subtitle = new Font("Arial", 14, FontStyle.Regular))
+            {
+                string gameOverText = "GAME OVER";
+                string retryText = "Press R to Restart";
+
+                SizeF titleSize = g.MeasureString(gameOverText, title);
+                SizeF retrySize = g.MeasureString(retryText, subtitle);
+
+                g.DrawString(
+                    gameOverText,
+                    title,
+                    Brushes.Red,
+                    (ClientSize.Width - titleSize.Width) / 2,
+                    220);
+
+                g.DrawString(
+                    retryText,
+                    subtitle,
+                    Brushes.White,
+                    (ClientSize.Width - retrySize.Width) / 2,
+                    280);
+            }
+        }
+
     }
 }
